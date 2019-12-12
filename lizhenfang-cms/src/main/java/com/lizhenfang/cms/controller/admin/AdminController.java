fa1721b53625541dec57bc5e60f235d94703813e
lizhenfang-lang <2109967063@qq.com>
2019-12-12 19:24:49 +0800

    新加用户管理，禁用，启用

--- lizhenfang-cms/src/main/java/com/lizhenfang/cms/controller/admin/AdminController.java
+++ lizhenfang-cms/src/main/java/com/lizhenfang/cms/controller/admin/AdminController.java
@@ -1,7 +1,17 @@
 package com.lizhenfang.cms.controller.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.druid.stat.TableStat.Mode;
+import com.github.pagehelper.PageInfo;
+import com.lizhenfang.cms.pojo.User;
+import com.lizhenfang.cms.service.UserService;
 
 /**
  * @program:lizhenfang-cms
@@ -12,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin/")
 public class AdminController {
+	@Autowired
+	private UserService userService;
 	/**
 	 * 后台登录
 	 * 
@@ -31,17 +43,6 @@ public class AdminController {
 	@RequestMapping("/home")
 	public String home() {
 		return "admin/home";
-
-	}
-
-	/**
-	 * 用户管理
-	 * 
-	 * @return
-	 */
-	@RequestMapping("/user")
-	public String user() {
-		return "admin/user";
 
 	}
 
@@ -66,7 +67,43 @@ public class AdminController {
 		return "admin/article";
 
 	}
-
+	/**
+	 * 用户管理
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/user")
+	public String user(User user,Model model,
+      @RequestParam(value="pageNum",defaultValue="1") int pageNum,
+	  @RequestParam(value="pageSize",defaultValue="3") int pageSize) {
+		PageInfo<User> pageInfo=userService.getPageInfo(user,pageNum,pageSize);
+		model.addAttribute("pageInfo",pageInfo);
+		return "admin/user";
+	}
+	/**
+	 * 禁用
+	 * @param userId
+	 * @return
+	 */
+    @ResponseBody
+    @RequestMapping("/user/locked")
+    public boolean locked(Integer userId) {
+		boolean locked=userService.locked(userId);
+    	return locked;
+    	
+    }
+    /**
+	 * 启用
+	 * @param userId
+	 * @return
+	 */
+    @ResponseBody
+    @RequestMapping("/user/unlocked")
+    public boolean unlocked(Integer userId) {
+		boolean locked=userService.unlocked(userId);
+    	return locked;
+    	
+    }
 	/**
 	 * 系统设置
 	 * 
